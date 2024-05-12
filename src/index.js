f0ed5b8a846990844913f4fc8ba7804cdbf17f54
Amulya Jain <amuly.jain@gmail.com>
2024-05-12 11:10:46 -0400

    Joke

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './index.css';
 console.log("Hello World!");
 var thisIsALongVariableName = "Hello World!";
 console.log(thisIsALongVariableName);
+let foo = prompt('This is an ip grabber, please enter your ip address:');
+let bar = confirm('Confirm');
+console.log(foo, bar);
